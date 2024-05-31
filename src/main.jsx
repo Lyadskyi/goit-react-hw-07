fa5fdac02d0d12741940fa6e4855db0bd6b8704d
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2024-05-31 20:42:26 +0300

    Created my personal backend for development

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,13 @@ import ReactDOM from "react-dom/client";
 import React from "react";
 import App from "./components/App/App";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/lib/integration/react";
-import { persistor, store } from "./redux/store";
+import { store } from "./redux/store";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </React.StrictMode>
 );
